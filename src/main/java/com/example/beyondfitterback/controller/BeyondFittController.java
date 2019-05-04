f6974fd8d833ca4a920bbd7cda9f345ee4682b1c
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2019-05-04 22:16:29 +0900

    Enable CORS requests from all origins.

--- src/main/java/com/example/beyondfitterback/controller/BeyondFittController.java
+++ src/main/java/com/example/beyondfitterback/controller/BeyondFittController.java
@@ -3,12 +3,14 @@ package com.example.beyondfitterback.controller;
 import com.example.beyondfitterback.entity.BeyondFitt;
 import com.example.beyondfitterback.service.BeyondFittService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController()
+@CrossOrigin(exposedHeaders = "Served-By")
 public class BeyondFittController {
 
     private BeyondFittService beyondFittService;
