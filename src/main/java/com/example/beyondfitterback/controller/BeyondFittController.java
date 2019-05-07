33c70cf775a9c0dce967222e975a213597c40068
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2019-05-07 23:54:48 +0900

    Change path url context path.

--- src/main/java/com/example/beyondfitterback/controller/BeyondFittController.java
+++ src/main/java/com/example/beyondfitterback/controller/BeyondFittController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController()
+@RestController
 @CrossOrigin(exposedHeaders = "Served-By")
 public class BeyondFittController {
 
